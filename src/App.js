53bb1013cf94838ac6c74d04a6c7862e953b43ea
Daria Khrushch <daria21khrushch@gmail.com>
2022-11-18 16:58:39 +0000

    section9
    React Context Continued

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
+import Checkout from './routes/checkout/checkout.component';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
@@ -13,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element ={<Checkout/>} />
       </Route>
     </Routes>
   );
