7a69f1e8e00811261f546b7745475afd4a5709a3
Daria Khrushch <daria21khrushch@gmail.com>
2022-11-21 10:40:10 +0000

    Section 15
     Redux Extended Tools

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ProductsProvider } from "./contexts/products.context";
 import { Provider } from "react-redux";
-import {store} from './store/store'
-
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-      <ProductsProvider>
-       
-          <App />
-        
-        </ProductsProvider>
-        </Provider>
+        <PersistGate persistor={persistor}>
+          <ProductsProvider>
+            <App />
+          </ProductsProvider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
